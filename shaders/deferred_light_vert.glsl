a83102188752021e64d531f82dd4b7c4a32cabf1
CJT <jietong_chen@outlook.com>
2016-06-23 17:00:49 +0800

    Update deferred_light_vert.glsl

--- shaders/deferred_light_vert.glsl
+++ shaders/deferred_light_vert.glsl
@@ -5,16 +5,15 @@ in vec2 p3d_MultiTexCoord0;
 
 out vec2 fTexCoord;
 
-uniform mat4 p3d_ModelMatrix;
-uniform mat4 p3d_ModelViewMatrix;
-uniform mat4 p3d_ModelViewProjectionMatrix;
+uniform vec2 TexScale;
 
 void main()
 {
 	//fPos = p3d_ModelViewMatrix * p3d_Vertex;
     //fPos.z = 0.0;
     //fTexCoord = p3d_Vertex.xz * 0.5 + 0.5;
-    fTexCoord = p3d_MultiTexCoord0 * vec2(0.625, 0.703125);
+    //fTexCoord = p3d_MultiTexCoord0 * vec2(0.625, 0.703125);
+    fTexCoord = p3d_MultiTexCoord0 * TexScale;
 
     //gl_Position = p3d_ModelViewProjectionMatrix * p3d_Vertex;
     vec4 p = p3d_Vertex.xzyw;
