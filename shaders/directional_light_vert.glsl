ddd724c88f7c340c3388760200b774d09117930b
CJT <jietong_chen@outlook.com>
2016-06-24 16:47:03 +0800

    Update directional_light_vert.glsl

--- shaders/directional_light_vert.glsl
+++ shaders/directional_light_vert.glsl
@@ -2,8 +2,12 @@
 
 in vec4 p3d_Vertex;
 in vec2 p3d_MultiTexCoord0;
+in vec3 p3d_Tangent;
+in vec3 p3d_Binormal;
 
 out vec2 fTexCoord;
+out vec3 fTangent;
+out vec3 fBinormal;
 
 uniform vec2 TexScale;
 
@@ -14,9 +18,11 @@ void main()
     //fTexCoord = p3d_Vertex.xz * 0.5 + 0.5;
     //fTexCoord = p3d_MultiTexCoord0 * vec2(0.625, 0.703125);
     fTexCoord = p3d_MultiTexCoord0 * TexScale;
+    fTangent = p3d_Tangent;
+    fBinormal = p3d_Binormal;
 
     //gl_Position = p3d_ModelViewProjectionMatrix * p3d_Vertex;
-    vec4 p = p3d_Vertex.xzyw;
-    p.w = 1.0;
-    gl_Position = p;
+    vec4 pos = p3d_Vertex.xzyw;
+    pos.w = 1.0;
+    gl_Position = pos;
 }
