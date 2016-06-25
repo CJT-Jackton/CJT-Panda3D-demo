ee865d99039d2ab91255480a7fae11733e62929f
CJT <jietong_chen@outlook.com>
2016-06-25 16:56:34 +0800

    Update point_light_frag.glsl

--- shaders/point_light_frag.glsl
+++ shaders/point_light_frag.glsl
@@ -1,14 +1,19 @@
 #version 330 core
 
-in vec2 fTexCoord;
+in vec4 fPos;
 
 layout (location = 0) out vec4 fColor;
+
+uniform mat4 p3d_ModelViewMatrix;
+uniform mat4 p3d_ViewMatrix;
 
 uniform sampler2D gDepthStencil;
 uniform sampler2D gDiffuse;
 uniform sampler2D gNormal;
 //uniform sampler2D gSpecular;
 //uniform sampler2D gIrradiance;
+uniform vec2 TexScale;
+uniform vec4 TranSStoVS;
 uniform struct p3d_LightSourceParameters {
   vec4 color;
   vec4 specular;
@@ -23,8 +28,22 @@ uniform struct p3d_LightSourceParameters {
 
 void main()
 {
-    vec4 diffuse = PointLight.color;
-    vec4 color = diffuse;
+    vec3 fPos_clip = fPos.xyz / fPos.w;
+    vec2 fTexCoord = (fPos_clip.xy * 0.5 + 0.5) * TexScale;
+
+    vec4 albedo = texture(gDiffuse, fTexCoord);
+    vec3 normal = (texture(gNormal, fTexCoord).rbg - 0.5) * 2;
+    float depth = texture(gDepthStencil, fTexCoord);
+
+    vec3 fPos_view = (fPos_clip.xzy * TranSStoVS.xyz) / (depth + TranSStoVS.w);
+
+    vec4 lpos_view = p3d_ViewMatrix * PointLight.position;
+    vec3 lvec = vec3(lpos_view) - fPos_view;
+    float ldistance = length(lvec);
+    vec3 ldir = normalize(lvec);
+
+    vec4 diffuse = PointLight.color * max(dot(normal, ldir), 0.0);
+    vec4 color = albedo * diffuse;
 
     fColor = color;
 }
