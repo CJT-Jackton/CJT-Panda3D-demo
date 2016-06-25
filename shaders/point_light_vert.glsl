9d72f750375da47e87fb918333728713664ff9b9
CJT <jietong_chen@outlook.com>
2016-06-25 16:56:09 +0800

    Update point_light_vert.glsl

--- shaders/point_light_vert.glsl
+++ shaders/point_light_vert.glsl
@@ -2,13 +2,13 @@
 
 in vec4 p3d_Vertex;
 
-out vec2 fTexCoord;
+out vec4 fPos;
 
 uniform mat4 p3d_ModelViewProjectionMatrix;
 
 void main()
 {
-	fTexCoord = vec2(0.0, 0.0);
-
+	fPos = p3d_ModelViewProjectionMatrix * p3d_Vertex;
+    fPos.z = fPos.w;
     gl_Position = p3d_ModelViewProjectionMatrix * p3d_Vertex;
 }
