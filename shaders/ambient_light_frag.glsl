158c071c5b25ad65e5e4bc03dafa02b19612c40f
CJT <jietong_chen@outlook.com>
2016-06-29 17:16:03 +0800

    Update ambient_light_frag.glsl

--- shaders/ambient_light_frag.glsl
+++ shaders/ambient_light_frag.glsl
@@ -4,18 +4,18 @@ in vec2 fTexCoord;
 
 layout (location = 0) out vec4 fColor;
 
-uniform sampler2D gDepthStencil;
-uniform sampler2D gDiffuse;
-uniform sampler2D gNormal;
-//uniform sampler2D gSpecular;
-//uniform sampler2D gIrradiance;
+uniform sampler2D TexDepthStencil;
+uniform sampler2D TexDiffuse;
+uniform sampler2D TexNormal;
+//uniform sampler2D TexSpecular;
+//uniform sampler2D TexIrradiance;
 uniform struct p3d_LightSourceParameters {
   vec4 color;
 } AmbientLight;
 
 void main()
 {
-    vec4 albedo = texture(gDiffuse, fTexCoord);
+    vec4 albedo = texture(TexDiffuse, fTexCoord);
     vec4 color = albedo * AmbientLight.color;
 
     fColor = color;
