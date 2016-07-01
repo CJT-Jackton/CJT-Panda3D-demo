42fdeff3445278e98d770cb506a8875306fd9f0f
CJT <jietong_chen@outlook.com>
2016-07-01 08:39:52 +0800

    Update smaa_edge_vert.glsl

--- shaders/smaa_edge_vert.glsl
+++ shaders/smaa_edge_vert.glsl
@@ -11,8 +11,14 @@ out vec4 offset[3];
 #define SMAA_GLSL_4 1
 #define SMAA_PRESET_ULTRA 1
 #define SMAA_INCLUDE_VS 1
+#define SMAA_INCLUDE_PS 0
 
-#include "SMAA.hlsl"
+void SMAAEdgeDetectionVS(vec2 texcoord,
+                         out vec4 offset[3]) {
+    offset[0] = fma(SMAA_RT_METRICS.xyxy, vec4(-1.0, 0.0, 0.0, -1.0), texcoord.xyxy);
+    offset[1] = fma(SMAA_RT_METRICS.xyxy, vec4( 1.0, 0.0, 0.0,  1.0), texcoord.xyxy);
+    offset[2] = fma(SMAA_RT_METRICS.xyxy, vec4(-2.0, 0.0, 0.0, -2.0), texcoord.xyxy);
+}
 
 uniform vec2 texScale;
 
