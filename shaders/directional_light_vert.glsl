453582aac578d703e17a4608e4c00dd7ed1fc968
CJT <jietong_chen@outlook.com>
2016-07-01 22:04:02 +0800

    Update directional_light_vert.glsl

--- shaders/directional_light_vert.glsl
+++ shaders/directional_light_vert.glsl
@@ -9,13 +9,8 @@ uniform vec2 texScale;
 
 void main()
 {
-	//fPos = p3d_ModelViewMatrix * p3d_Vertex;
-    //fPos.z = 0.0;
-    //fTexCoord = p3d_Vertex.xz * 0.5 + 0.5;
-    //fTexCoord = p3d_MultiTexCoord0 * vec2(0.625, 0.703125);
     fTexCoord = p3d_MultiTexCoord0 * texScale;
 
-    //gl_Position = p3d_ModelViewProjectionMatrix * p3d_Vertex;
     vec4 pos = p3d_Vertex.xzyw;
     pos.w = 1.0;
     gl_Position = pos;
