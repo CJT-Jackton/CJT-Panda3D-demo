5c14468fa871427b8c45637185bdea42e332e1f3
CJT <jietong_chen@outlook.com>
2016-07-05 08:19:17 +0800

    Update ssao_noise_vert.glsl

--- shaders/ssao_noise_vert.glsl
+++ shaders/ssao_noise_vert.glsl
@@ -3,13 +3,14 @@
 in vec4 p3d_Vertex;
 in vec2 p3d_MultiTexCoord0;
 
+out vec4 fPos;
 out vec2 fTexCoord;
 
 void main()
 {
     fTexCoord = p3d_MultiTexCoord0;
 
-    vec4 pos = p3d_Vertex.xzyw;
-    pos.w = 1.0;
-    gl_Position = pos;
+    vec4 fPos = p3d_Vertex.xzyw;
+    fPos.w = 1.0;
+    gl_Position = fPos;
 }
