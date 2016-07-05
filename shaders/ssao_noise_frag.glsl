9c783bd30b9034b5d2224a13e253896cd8971fdf
CJT <jietong_chen@outlook.com>
2016-07-05 17:01:08 +0800

    Update ssao_noise_frag.glsl

--- shaders/ssao_noise_frag.glsl
+++ shaders/ssao_noise_frag.glsl
@@ -18,25 +18,25 @@ uniform sampler2D TexNoise;
 uniform vec4 samples[64];
 
 int kernelSize = 64;
-float radius = 3.0;
+float radius = 1.0;
 
 const float NEAR = 1.0; // Projection matrix's near plane distance
 const float FAR = 500.0; // Projection matrix's far plane distance
 float LinearizeDepth(float depth)
 {
     float z = depth * 2.0 - 1.0;
-    return (2.0 * NEAR * FAR) / (FAR + NEAR - z * (FAR - NEAR));    
+    return (2.0 * NEAR * FAR) / (FAR + NEAR - z * (FAR - NEAR));
 }
 
 void main()
 {
-	float depth = texture(TexDepthStencil, fTexCoord * texScale).r;
-    vec4 tmp = p3d_ProjectionMatrixInverse * vec4(fPos.x, fPos.y, (depth * 2 - 1.0), 1.0);
+	float depth = texture(TexDepthStencil, fTexCoord * texScale).rgb;
+    vec4 tmp = p3d_ProjectionMatrixInverse * vec4((fTexCoord.x * 2 - 1.0), (fTexCoord.y * 2 - 1.0), (depth * 2 - 1.0), 1.0);
     vec3 fPos_view = tmp.xyz / tmp.w;
 
-    vec3 normal = normalize(texture(TexNormal, fTexCoord * texScale).rbg * 2 - 1);
+    vec3 normal = texture(TexNormal, fTexCoord * texScale).rbg * 2 - 1;
 
-    vec3 randomVec = normalize(texture(TexNoise, fTexCoord * texScaleNoise).xyz * 2 - 1);
+    vec3 randomVec = normalize(texture(TexNoise, fTexCoord * texScaleNoise).xyz);
 
     // Create TBN change-of-basis matrix: from tangent-space to view-space
     vec3 tangent = normalize(randomVec - normal * dot(randomVec, normal));
@@ -47,7 +47,7 @@ void main()
 
     for(int i = 0; i < kernelSize; ++i)
     {
-        vec3 sampleVec = TangentMatrix * samples[i].xyz;
+        vec3 sampleVec = TangentMatrix * samples[i].xzy;
         vec3 sample_view = fPos_view + sampleVec * radius;
 
         vec4 offset = vec4(sample_view, 1.0);
@@ -55,12 +55,18 @@ void main()
         offset.xyz /= offset.w;
         offset.xyz = offset.xyz * 0.5 + 0.5;
 
-        float sample_depth = -texture(TexDepthStencil, offset.xy * texScale).r;
+        float sample_depth = texture(TexDepthStencil, offset.xy * texScale).rgb;
+        //sample_depth = sample_depth == 0.0 ? FAR: sample_depth;
+        sample_depth = -LinearizeDepth(sample_depth);
+        //sample_depth = -(sample_depth * 2 - 1.0);
 
-        float rangeCheck = smoothstep(0.0, 1.0, radius / abs(fPos_view.z - sample_depth));
+        //float rangeCheck = smoothstep(0.0, 1.0, radius / abs(fPos_view.z - sample_depth));
+        //float rangeCheck = clamp(radius / abs(fPos_view.z - sample_depth), 0.0, 1.0);
+        float rangeCheck = 1.0;
         
-        if(LinearizeDepth(sample_depth) >= sample_view.z)
-            AmbientOcclusion += 1.0 * rangeCheck;
+        //if(sample_depth >= sample_view.z)
+        //    AmbientOcclusion += 1.0 * rangeCheck;
+        AmbientOcclusion += (sample_depth >= sample_view.z? 1.0: 0.0) * rangeCheck;
     }
 
     AmbientOcclusion = 1.0 - (AmbientOcclusion / 64.0);
