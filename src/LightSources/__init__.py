e2ab5bbffb58e6c33f72a43e3957879a17ee9d3b
CJT <jietong_chen@outlook.com>
2016-07-14 09:39:05 +0800

    Create __init__.py

---
+++ src/LightSources/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# Author: CJT
+# Date: 2016/7/13
+
+from IWAmbientLight import IWAmbientLight
+from IWDirectionalLight import IWDirectionalLight
+from IWPointLight import IWPointLight
+from IWSpotlight import IWSpotlight
